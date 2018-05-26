558399a4d1710c975e5dc5fe63533b613e790e1d
contrbas@gmail.com <K4rmU78B>
2018-05-26 11:25:28 +0300

    ALL

--- ncurses/hello_ncurses.c
+++ ncurses/hello_ncurses.c
@@ -45,5 +45,46 @@ int		main(void)
  	int c = getch(); // whaits till we press a key, returns int value of that key;
 
  	endwin(); //fre the memory on the init screen; 	
+
+
+ 	// cursor is used to place a necessary object;
+ 	// think it a layers;
+ 	// mpves the cursor to a specified location;
+ 	int x, y;
+ 	x = y = 10;
+ 	move(y,x);
+ 	printw("smth"); will print at y,x;
+ 	mvprintw(y,x, something);
+ 	clear();
+
+ 	//clears the screen;
+ 	clear();
+
+ 	WINDOW * newwin(height, width, start_y, start_x); // window pointer -> create a window;
+ 	refresh();
+
+ 	box(win, 0, 0); // set a default box;
+
+ 	// customize box;
+ 	char c = 'g'
+ 	box(win, (int) c, 103); // border of g
+
+ 	int left, right, ... , brc;
+ 	left = right = 103;
+ 	wborder(wun, left, right, top, bottom, tlc, trc, blc, brc);
+
+ 	mwprintw(win, "this is my box"); // print the war at a specific place;
+ 	wrefresh(); // refresh only a window specified;
+
+ 	cbreak(); // cnrl + c ->exit prog; // has as default;
+ 	// mask the user input;
+ 	raw(); // show the input;
+ 	noecho(); // will not show the user input;
+
+
+
+
+
+
 	return (0);
 }

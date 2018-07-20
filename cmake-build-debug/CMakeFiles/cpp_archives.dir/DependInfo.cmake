376e40afad462024f1cee15a9f819e4cf0b41059
contrbas@gmail.com <K4rmU78B>
2018-07-20 11:38:40 +0300

    ALL

---
+++ cmake-build-debug/CMakeFiles/cpp_archives.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/otimofie/Projects/cpp_archives/ncurses/alienwave/aliens.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/alienwave/aliens.c.o"
+  "/Users/otimofie/Projects/cpp_archives/ncurses/alienwave/blit.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/alienwave/blit.c.o"
+  "/Users/otimofie/Projects/cpp_archives/ncurses/alienwave/fire.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/alienwave/fire.c.o"
+  "/Users/otimofie/Projects/cpp_archives/ncurses/alienwave/levels.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/alienwave/levels.c.o"
+  "/Users/otimofie/Projects/cpp_archives/ncurses/alienwave/main.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/alienwave/main.c.o"
+  "/Users/otimofie/Projects/cpp_archives/ncurses/alienwave/shield.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/alienwave/shield.c.o"
+  "/Users/otimofie/Projects/cpp_archives/ncurses/alienwave/util.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/alienwave/util.c.o"
+  "/Users/otimofie/Projects/cpp_archives/ncurses/alienwave/xzarna.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/alienwave/xzarna.c.o"
+  "/Users/otimofie/Projects/cpp_archives/ncurses/hello_ncurses.c" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/ncurses/hello_ncurses.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/01_hello_SDL.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/01_hello_SDL.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/02_getting_an_image_on_the_screen.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/02_getting_an_image_on_the_screen.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/03_event_driven_programming.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/03_event_driven_programming.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/04_key_presses.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/04_key_presses.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/05_optimized_surface_loading_and_soft_stretching.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/05_optimized_surface_loading_and_soft_stretching.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/06_extension_libraries_and_loading_other_image_formats.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/06_extension_libraries_and_loading_other_image_formats.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/07_texture_loading_and_rendering.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/07_texture_loading_and_rendering.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/08_geometry_rendering.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/08_geometry_rendering.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/09_the_viewport.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/09_the_viewport.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/01_hello_SDL-2/16_true_type_fonts.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/16_true_type_fonts.cpp.o"
+  "/Users/otimofie/Projects/cpp_archives/CDL info/first_SDL.cpp" "/Users/otimofie/Projects/cpp_archives/cmake-build-debug/CMakeFiles/cpp_archives.dir/CDL_info/first_SDL.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cpp_archives.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/01_hello_SDL.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/02_getting_an_image_on_the_screen.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/03_event_driven_programming.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/04_key_presses.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/05_optimized_surface_loading_and_soft_stretching.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/06_extension_libraries_and_loading_other_image_formats.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/07_texture_loading_and_rendering.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/08_geometry_rendering.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/09_the_viewport.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/01_hello_SDL-2/16_true_type_fonts.cpp.o"
+  "CMakeFiles/cpp_archives.dir/CDL_info/first_SDL.cpp.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/alienwave/aliens.c.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/alienwave/blit.c.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/alienwave/fire.c.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/alienwave/levels.c.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/alienwave/main.c.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/alienwave/shield.c.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/alienwave/util.c.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/alienwave/xzarna.c.o"
+  "CMakeFiles/cpp_archives.dir/ncurses/hello_ncurses.c.o"
+  "cpp_archives.pdb"
+  "cpp_archives"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/cpp_archives.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
